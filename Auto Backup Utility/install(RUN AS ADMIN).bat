85ba304229879422c993d85d5cdc08343824c51f
IWickGames <smarttecgaming@gmail.com>
2019-09-27 22:14:38 -0500

    Simple Update

--- Auto Backup Utility/install(RUN AS ADMIN).bat
+++ Auto Backup Utility/install(RUN AS ADMIN).bat
@@ -1,5 +1,6 @@
 @echo off
 echo Starting install of Auto Backup Utility
+cd "%~dp0"
 start "Auto Backup Utility Installer" /wait /MIN "installfiles\install1.bat"
 echo Install complete...
 pause >nul
